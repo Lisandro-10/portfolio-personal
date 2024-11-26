b4669876212172bcfee6b0de79c23fdb2e5ae2b5
Lisandro-10 <lisandroandia14@gmail.com>
2024-11-26 16:37:28 -0300

    hero section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "@/assests/icons/menu";
 
 export const Header = () => {
   return (
-    <header className="sticky top-0">
+    <header className="sticky top-0 backdrop-blur-sm z-20">
       <div className="flex justify-center items-center py-3 bg-black text-white text-sm gap-3">
         <p className="text-white/60 hidden md:block">
           Streamline your workflow and boost your productivity.
@@ -18,7 +18,7 @@ export const Header = () => {
         </div>
       </div>
       <div className="py-5 px-5">
-        <div className="container">
+        <div className="header-container">
           <div className="flex items-center justify-between">
             <Image src={Logo} alt="Saas Logo" height={40} width={40} />
             <div className="h-5 w-5 md:hidden">
